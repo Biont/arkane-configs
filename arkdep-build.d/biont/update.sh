46ba9e8c308b85b3c0bbbb3c565c294c5bf09e34
Moritz Meißelbach <arbelzapf@gmail.com>
2024-10-30 21:06:40 +0100

    Update update.sh

--- arkdep-build.d/biont/update.sh
+++ arkdep-build.d/biont/update.sh
@@ -3,3 +3,7 @@ if [[ -f $arkdep_boot/loader/entries/${data[0]}.conf ]]; then
 	mv $arkdep_boot/loader/entries/${data[0]}.conf $arkdep_boot/loader/entries/$(date +%Y%m%d-%H%M%S)-${data[0]}+3.conf
 	bootctl set-default ''
 fi
+
+if [[ -e /var/lib/fprint ]]; then
+	cp -rT /var/lib/fprint $arkdep_dir/deployments/${data[0]}/var/lib/fprint
+fi
