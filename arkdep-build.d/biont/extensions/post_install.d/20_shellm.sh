d99e8fb50ea56b233758ab2e768ecc422f59e14f
Moritz Meißelbach <arbelzapf@gmail.com>
2025-09-07 22:31:34 +0200

    📦 Update shellm installation script to use arch-chroot.

--- arkdep-build.d/biont/extensions/post_install.d/20_shellm.sh
+++ arkdep-build.d/biont/extensions/post_install.d/20_shellm.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/Biont/shellm/refs/heads/main/shellm -o /usr/bin/shellm
-chmod +x /usr/bin/shellm
+arch-chroot $workdir bash -c "wget https://raw.githubusercontent.com/Biont/shellm/refs/heads/main/shellm -o /usr/local/bin/shellm"
+arch-chroot $workdir bash -c "chmod +x /usr/local/bin/shellm"
