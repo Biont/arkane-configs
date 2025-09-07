e6c413a6d759be97c5ab21479046f8d3eedd9260
Moritz Meißelbach <arbelzapf@gmail.com>
2025-09-07 22:48:39 +0200

    🔥 Fix: Corrected output option in shellm download command.

--- arkdep-build.d/biont/extensions/post_install.d/20_shellm.sh
+++ arkdep-build.d/biont/extensions/post_install.d/20_shellm.sh
@@ -1,2 +1,2 @@
-arch-chroot $workdir bash -c "wget https://raw.githubusercontent.com/Biont/shellm/refs/heads/main/shellm -o /usr/local/bin/shellm"
+arch-chroot $workdir bash -c "wget https://raw.githubusercontent.com/Biont/shellm/refs/heads/main/shellm -O /usr/local/bin/shellm"
 arch-chroot $workdir bash -c "chmod +x /usr/local/bin/shellm"
